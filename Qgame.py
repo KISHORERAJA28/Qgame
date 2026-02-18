b027cd2992fb07eea97534daed58634188287f94
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-02-18 23:07:39 +0530

    Update and rename Qgame to Qgame.py

--- Qgame.py
+++ Qgame.py
@@ -1,11 +1,14 @@
 print(" Welcome To My Quiz Game \n Interesting Game to Play")
+
 Player = input(" Do you want to play the game? \n" )
 if Player.lower() != 'yes':
     print("Good Bye")
     quit()  
 name_player = input("Enter Your Name: ")
 print("Let's Start the Game :) ",name_player)
+
 score = 0
+
 answer = input(' What is CPU stands for? \n ')
 if answer.lower() == 'central processing unit':
     print("Correct")
@@ -18,18 +21,21 @@ if answer.lower() == 'graphical processing unit':
     score += 1
 else:
     print('Wrong')
+
 answer = input(' What is RAM stands for? \n ')
 if answer.lower() == 'random access memory':
     print("Correct")
     score += 1
 else:
     print('Wrong')
+
 answer = input(' What is ROM stands for? \n ')
 if answer.lower() == 'read only memory':
     print("Correct")
     score += 1
 else:
     print('Wrong')
+
 answer = input(' Mouse is an input device or output device? \n ')
 if answer.lower() == 'input device':
     print("Correct")
